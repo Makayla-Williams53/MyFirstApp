da0d900683b50c2c78ada12e180022b72d528423
makayla <makaylawms0426@gmail.com>
2022-08-22 12:23:03 -0700

    Have created first screen layout with images and textViews.

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+/* art credit
+    1) https://papik.pro/en/drawings/cute/147-cute-mushroom-drawing-45-photos.html
+ */
