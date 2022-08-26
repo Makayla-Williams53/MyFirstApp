5ac9ab587dbdf93c89df7c2630490712a65a72df
makayla <makaylawms0426@gmail.com>
2022-08-26 12:38:04 -0700

    Have created the splash screen

--- app/src/main/java/com/example/myfirstapp/SplashScreen.java
+++ app/src/main/java/com/example/myfirstapp/SplashScreen.java
@@ -2,7 +2,10 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent intentMain = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intentMain);
+            }//end run method
+        }, 5000); //end post delay
+
+    }// end on Create
+}//end
